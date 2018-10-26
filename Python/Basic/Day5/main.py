7ac7203fa59516f60c59fb525c1726cafc5b6f5f
DTVThe <dtvthe@gmail.com>
2018-10-26 23:09:37 +0700

    Homework Day5 Function_File

--- Python/Basic/Day5/main.py
+++ Python/Basic/Day5/main.py
@@ -1,2 +1,18 @@
 import text_utility
-print(text_utility.read())
+
+file_lyrics = 'lyrics.txt'
+file_data = 'data.txt'
+
+str_lyrics = text_utility.read(file_lyrics)
+print(str_lyrics)
+
+number_of_lines = text_utility.count_by_lines(file_lyrics)
+number_of_words = text_utility.count_by_words(file_lyrics)
+number_of_chars = text_utility.count_by_chars(file_lyrics)
+print('Number of lines: ', number_of_lines)
+print('Number of words: ', number_of_words)
+print('Number of chars: ', number_of_chars)
+
+text_utility.erase(file_lyrics)
+
+text_utility.write(file_data, str_lyrics)
--- Python/Basic/Day5/text_utility.py
+++ Python/Basic/Day5/text_utility.py
@@ -1,29 +1,60 @@
-file = None
-
-def init(file_path):
-    global file
-    file = open(file_path)
-
-
-def close():
-    pass
-FILE_PATH = 'data.txt'
-def read():
-    file = open(FILE_PATH)
-    lines = file.read()
+def read(str_path_file):
+    """
+    Read all text from text file
+    :param str_path_file: Text file path to read
+    :return: All text in your text file
+    """
+    file = open(str_path_file, 'r')
+    text = file.read()
     file.close()
-    return lines
+    return text
 
-def write():
-    pass
 
-def count_by_lines():
-    pass
+def write(str_path_file, text_content):
+    """
+    Write content to text file
+    :param str_path_file: Text file path to write
+    :param text_content: Content text to write
+    """
+    file = open(str_path_file, 'w')
+    file.write(text_content)
+    file.close()
 
-def count_by_words():
-    pass
 
-def count_by_chars():
-    pass
-def erase():
-    pass
+def count_by_lines(str_path_file):
+    """
+    Number of lines in your text file
+    :param str_path_file: Text file path to count by lines
+    :return: Number of lines
+    """
+    return len(read(str_path_file).split('\n'))
+
+
+def count_by_words(str_path_file):
+    """
+    Number of words in your text file
+    :param str_path_file: Text file path to count by words
+    :return: Number of words
+    """
+    number_of_word = 0
+    for word in read(str_path_file).replace('\n',' ').split(' '):
+        if word is not '':
+            number_of_word += 1
+    return number_of_word
+
+
+def count_by_chars(str_path_file):
+    """
+    Number of characters in your text file
+    :param str_path_file: Text file path to count by characters
+    :return: Number of characters
+    """
+    return len(read(str_path_file))
+
+
+def erase(str_path_file):
+    """
+    Delete content in your text file
+    :param str_path_file: Text file path to delete
+    """
+    write(str_path_file, '')
