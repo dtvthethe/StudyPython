866c415dc5ab1d649ebf4a3f383f926cdff0de16
Đàm Thế <damthe@ams-MacBook-Pro.local>
2018-11-20 01:21:10 +0700

    Remove mysite

--- mysite/polls/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class QuestionForm(forms.Form):
-    question_text = forms.CharField(max_length=100, label='Question Label')
-    email = forms.EmailField(error_messages={'invalid':'Oops! Wrong format Email :D'})
