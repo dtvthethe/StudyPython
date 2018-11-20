5e610ceb83d773bce54380ecc8bd714f21ec5bd7
DTVThe <dtvthe@gmail.com>
2018-11-21 03:31:21 +0700

    static file config

--- trydjango/trydjango/urls.py
+++ trydjango/trydjango/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',include('posts.urls', namespace='abc')),
-    path('cat', include('categories.urls')),
-    path('admin/', admin.site.urls),
-]
+                  path('', include('posts.urls', namespace='abc')),
+                  path('cat', include('categories.urls')),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
