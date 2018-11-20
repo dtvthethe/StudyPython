26149ecf115ace35f50d190bb3049c7972000618
DTVThe <dtvthe@gmail.com>
2018-11-21 04:55:07 +0700

    paging

--- trydjango/posts/views.py
+++ trydjango/posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.contrib import messages
 from .models import Post
 from .forms import PostForm
@@ -27,9 +28,13 @@ def detail(request, id = None):
     return render(request, 'post/detail.html', context= context)
 
 def list(request):
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 5)
+    page = request.GET.get('page')
+    data_rows = paginator.get_page(page)
     context = {
         'title': 'Post List',
-        'data_rows': Post.objects.all()
+        'data_rows': data_rows
     }
     return render(request, 'post/index.html', context=context)
 
